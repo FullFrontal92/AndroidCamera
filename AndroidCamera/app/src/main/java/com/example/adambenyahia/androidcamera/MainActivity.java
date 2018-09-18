9d0a42c02ee66d468e2f22eee1b7a54bc2eec0ab
FullFrontal92 <adam.benyahia@hotmail.fr>
2018-09-18 14:48:11 +0200

    woking app

--- AndroidCamera/app/src/main/java/com/example/adambenyahia/androidcamera/MainActivity.java
+++ AndroidCamera/app/src/main/java/com/example/adambenyahia/androidcamera/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.adambenyahia.androidcamera;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.media.Image;
+import android.os.Build;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import static android.widget.ImageView.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +29,46 @@ public class MainActivity extends AppCompatActivity {
     int tag=1;
 
     public void buTakePicture(View view) {
+        CheckUserPermsions();
+    }
+
+    void CheckUserPermsions(){
+        if ( Build.VERSION.SDK_INT >= 23){
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+                    PackageManager.PERMISSION_GRANTED  ){
+                requestPermissions(new String[]{
+                                android.Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        REQUEST_CODE_ASK_PERMISSIONS);
+                return ;
+            }
+        }
+
+        TakePicture();
+
+    }
+    //get acces to location permsion
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    TakePicture();// init the contact list
+                } else {
+                    // Permission Denied
+                    Toast.makeText( this,"impossible to store the file" , Toast.LENGTH_SHORT)
+                            .show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+     void TakePicture(){
         Intent intent= new Intent();
         intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(intent,tag);
@@ -28,6 +76,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode,Intent data){
-
+        if (requestCode==tag && resultCode==RESULT_OK) {
+            Bundle b=data.getExtras();
+            Bitmap img=(Bitmap)b.get("data");
+            imageView.setImageBitmap(img);
+        }
     }
 }
